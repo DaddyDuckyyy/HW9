dcc1d735f6e681e35a954bd9fff0c0075b913f98
Daria Orlova <da.orlova365@gmail.com>
2024-02-21 17:50:45 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] ints = generateRandomArray();
-        int result = 0;
-        for (int anInt : ints) {
-            result += anInt;
-        }
-        System.out.println(result);
+        task1();
     }
 
     public static int[] generateRandomArray() {
@@ -17,5 +12,13 @@ public class Main {
             arr[i] = random.nextInt(100_001) + 100_000;
         }
         return arr;
+    }
+    public static void task1(){
+        int[] arr = generateRandomArray();
+        int result = 0;
+        for (int anInt : arr) {
+            result += anInt;
+        }
+        System.out.println(result);
     }
 }
