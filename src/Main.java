c8ef1e8aa19a8afaee370101ba888416a2f6e89b
Daria Orlova <da.orlova365@gmail.com>
2024-02-21 18:19:11 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         task1();
+        task23();
+        task4();
     }
 
     public static int[] generateRandomArray() {
@@ -13,12 +15,40 @@ public class Main {
         }
         return arr;
     }
-    public static void task1(){
+
+    public static void task1() {
         int[] arr = generateRandomArray();
         int result = 0;
         for (int anInt : arr) {
             result += anInt;
         }
-        System.out.println(result);
+        System.out.println("Сумма выплат за месяц " + result + " p.");
+    }
+
+    public static void task23() {
+        int[] expenses = generateRandomArray();
+        int totalExpenses = 0;
+        int minExpenses = expenses[0];
+        int maxExpenses = expenses[0];
+        for (int expense : expenses) {
+            totalExpenses += expense;
+            if (expense < minExpenses) {
+                minExpenses = expense;
+            }
+            if (expense > maxExpenses) {
+                maxExpenses = expense;
+            }
+        }
+        System.out.println("Сумма трат за месяц: " + totalExpenses + " p.");
+        System.out.println("Минимальная сумма трат в день: " + minExpenses + " p.");
+        System.out.println("Максимальная сумма трат в день: " + maxExpenses + " p.");
+        System.out.println("Средняя сумма трат за месяц: " + (double) totalExpenses / expenses.length + " p.");
+    }
+
+    public static void task4() {
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 }
